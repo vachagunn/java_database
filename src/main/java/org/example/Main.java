e4aad4b5803a3d8d1a2bb620a011e83195bda8c5
vachagunn <vachikpuper@gmail.com>
2023-12-09 14:59:36 +0500

    Create table "task"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,28 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:sqlite:cats.db";
+        String url = "jdbc:sqlite:database.db";
         Connection connection = DriverManager.getConnection(url);
+
+        // table task
+        // id, title, description, time, difficult, isDone
+        Statement statement = connection.createStatement();
+        statement.execute("""
+                CREATE TABLE task (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT,
+                    description TEXT,
+                    time INTEGER,
+                    difficult INTEGER,
+                    is_done INTEGER
+                )
+                """);
+
+        statement.close();
+        connection.close();
     }
 }
